335c2cc9e84f08097f76edde71f73558f45497df
zengdehao <972246473@qq.com>
2019-04-14 10:10:07 +0800

    优化统计pass和fail用例的个数

---
+++ case_test.py
@@ -0,0 +1,22 @@
+import time
+from base.HTMLTestRunner import HTMLTestRunner
+import unittest
+import os
+
+# 制定测试用例的文件目录
+test_dir = os.path.dirname(os.path.abspath(__file__))
+discover = unittest.defaultTestLoader.discover(test_dir, pattern='*_test.py')
+if __name__ == '__main__':
+    now_time = time.strftime("%Y-%m-%d-%H_%M_%S",time.localtime(time.time()))
+    file_name =os.path.dirname(os.path.abspath(__file__)) + '\\report\\' + now_time + '_result.html'
+    fp = open(file_name,'wb')
+    runner = HTMLTestRunner(stream=fp,
+                            title='Tvyuetravel Platform TestResult',
+                            description='Implementation Example with: ')
+    runner.run(discover)
+    fp.close()
+    print("\nsend success")
+
+
+
+
---
+++ main/run_test.py
@@ -0,0 +1,83 @@
+import sys
+import os
+sys.path.append('./base')
+sys.path.append('./data')
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,parentdir)
+from base.runmethod import RunMethod
+from data.getdata import GetData
+from data.dependent_data import DependentData
+from common.send_email import SendEmail
+import operator
+import unittest
+
+class RunTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.run_method = RunMethod()
+        cls.data = GetData()
+        cls.pass_count = []
+        cls.fail_count = []
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.send = SendEmail(cls.pass_count,cls.fail_count)
+        cls.send.send_email()
+
+# 执行用例的方法体
+    def run_test(self,row):
+        is_run = self.data.get_is_run(row)
+        if is_run:
+            url = self.data.get_request_url(row)
+            method = self.data.get_request_method(row)
+            request_data = self.data.get_request_data(row)
+            headers = self.data.get_is_header(row)
+            expect = self.data.get_expect_data(row)
+            print(type(expect),expect)
+            depend_case = self.data.get_depend_case(row)
+            print(type(depend_case))
+            # 判断是否存在依赖数据
+            if depend_case != '':
+                self.depend_data = DependentData(depend_case)
+                depend_reponse_value = self.depend_data.get_dependent_values(row)  # 获取依赖接口返回的数据的依赖的key对应的value值
+                depend_key = self.data.get_field_depend(row)  # 获取依赖数据value所对应的新的接口的key
+                request_data[depend_key] = depend_reponse_value  # key=value
+
+            # 发送请求
+            res = self.run_method.run_main(method,url,request_data,headers)
+
+            # 预期结果与实际结果对比
+            # result = operator.contains(res,expect)
+            try:
+                result = self.assertIn(expect,res)
+                self.data.write_result(row, "pass")
+                self.pass_count.append(row)
+                print(type(res), res)
+                print(result)
+                print(self.pass_count)
+                print("第%d条用例执行完毕" % row)
+            except AssertionError as e:
+                self.data.write_result(row, res)
+                self.fail_count.append(row)
+                print(self.fail_count)
+                raise
+
+    @staticmethod
+    def get_case_all(row):
+        def get_case(self):
+            self.run_test(row)
+        return get_case
+
+# 根据Excel用例的数量批量生成以“test”开头的测试方法
+def add_test_method():
+    data = GetData()
+    rows_count = data.get_case_lines()
+    for row in range(1, rows_count):
+        setattr(RunTest, "test_%d" % row, RunTest.get_case_all(row))
+
+# 生成test方法
+add_test_method()
+
+
+if __name__ == '__main__':
+    unittest.main()
